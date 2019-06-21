cf72754d05b4822ab7e0538aff713f0a2ff97b99
haoShen3 <2457937678@qq.com>
2019-06-21 16:40:50 +1000

    velocity

--- src/main/java/com/nowcoder/controller/indexController.java
+++ src/main/java/com/nowcoder/controller/indexController.java
@@ -1,11 +1,15 @@
 package com.nowcoder.controller;
 
 
+import com.nowcoder.model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class indexController {
@@ -22,5 +26,23 @@ public class indexController {
     public String profile(@PathVariable("userId") int userId,
                           @RequestParam("key") String key){
         return String.format("Profile page of %d %s", userId, key);
+    }
+
+    @RequestMapping(path = "/vm", method={RequestMethod.GET})
+    public String template(Model model){
+        model.addAttribute("value1", "123");
+        model.addAttribute("value2", "3456");
+        List<String> colors = Arrays.asList(new String[]{"RED, GREEN", "BLUE"});
+        model.addAttribute("colors", colors);
+
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < 4; ++i){
+            map.put(i, i * i);
+        }
+        model.addAttribute("map", map);
+
+        User user = new User("john");
+        model.addAttribute("user", user);
+        return "home";
     }
 }
